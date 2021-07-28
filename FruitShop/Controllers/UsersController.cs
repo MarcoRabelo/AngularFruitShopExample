fe4148e1dfae0bb1f3dcf19f52f3a04210d22140
DESKTOP-4CS80FC\marco <marco.rabelo@gmail.com>
2021-07-28 19:08:59 -0300

    Adding Angular Project.

---
+++ FruitShop/Controllers/UsersController.cs
@@ -0,0 +1,149 @@
+﻿using FruitShop.Application.Interfaces;
+using FruitShop.Application.ViewModels;
+using FruitShop.Auth.Services;
+using FruitShop.WebAPI.Filters;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Security.Claims;
+
+namespace FruitShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController, Authorize]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        // GET: api/<UsersController>
+        [HttpGet]
+        public IActionResult Get([FromQuery] PaginationFilter filter)
+        {
+            return Ok(_userService.Get(filter.PageNumber, filter.PageSize));
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            try
+            {
+                return Ok(_userService.GetById(id));
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        // POST api/<UsersController>
+        [HttpPost, AllowAnonymous]
+        public IActionResult Post([FromBody] UserViewModel userViewModel)
+        {
+            try
+            {
+                userViewModel.Admin = false;
+                _userService.Insert(userViewModel);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return Ok();
+        }
+
+        // PUT api/<UsersController>
+        [HttpPut]
+        public IActionResult Put([FromBody] UserViewModel userViewModel)
+        {
+            try
+            {
+                userViewModel.Id = Convert.ToInt64(TokenService.GetValueFromClaim(HttpContext.User.Identity, ClaimTypes.NameIdentifier));
+                _userService.Update(userViewModel);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return Ok();
+        }
+
+        // DELETE api/<UsersController>
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            try
+            {
+                //Delete your own account
+                return Ok(_userService.Delete(Convert.ToInt64(TokenService.GetValueFromClaim(HttpContext.User.Identity, ClaimTypes.NameIdentifier))));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                if (Convert.ToBoolean(TokenService.GetValueFromClaim(HttpContext.User.Identity, ClaimTypes.Role)))
+                {
+                    //Admin delete someone's account
+                    return Ok(_userService.Delete(id));
+                }
+                else
+                {
+                    return Unauthorized();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // POST api/<UsersController>/authenticate
+        [HttpPost("authenticate"), AllowAnonymous]
+        public IActionResult Authenticate([FromBody] UserAuthenticateRequestViewModel userAuthenticateRequest)
+        {
+            try
+            {
+                return Ok(_userService.Authenticate(userAuthenticateRequest));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // POST api/<UsersController>/newadmin
+        [HttpPost("newadmin")]
+        public IActionResult NewAdmin([FromBody] UserViewModel userViewModel)
+        {
+            try
+            {
+                if (Convert.ToBoolean(TokenService.GetValueFromClaim(HttpContext.User.Identity, ClaimTypes.Role)))
+                {
+                    _userService.Insert(userViewModel);
+                }
+                else
+                {
+                    return Unauthorized();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return Ok();
+        }
+    }
+}
---
+++ FruitShop/Startup.cs
@@ -0,0 +1,125 @@
+using FruitShop.Application.AutoMapper;
+using FruitShop.Auth.Models;
+using FruitShop.Data.Context;
+using FruitShop.IoC;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Text;
+
+namespace FruitShop
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
+
+            services.AddDbContext<FruitContext>(a => a.UseSqlServer(Configuration.GetConnectionString("FruitSQL")).EnableSensitiveDataLogging());
+            DependencyInjector.RegisterServices(services);
+            DependencyInjector.RegisterRepositories(services);
+            services.AddAutoMapper(typeof(AutoMapperConfiguration));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "FruitShop",
+                    Version = "v1",
+                    Description = "Simples API de exemplo para loja de frutas.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Marco Rabelo",
+                        Email = "marco.rabelo@gmail.com",
+                        Url = new Uri("https://www.youtube.com/channel/UC05P95nXawYc15gIpN8GFPw")
+                    }
+                });
+            });
+
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FruitShop v1"));
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            if (!env.IsDevelopment())
+            {
+                app.UseSpaStaticFiles();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
+        }
+    }
+}
